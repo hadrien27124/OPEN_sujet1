e1299e7544caaf440298271b2bf3ab7b51f6a449
Esteban Faravellon <efaravellon@etu.isara.fr>
2025-02-10 17:00:19 +0100

    Pop up

---
+++ ModifEsteban/ui.R
@@ -0,0 +1,10 @@
+library(shiny)
+library(leaflet)
+
+ui <- fluidPage(
+  titlePanel("Carte des Associations en France"),
+  
+  # Affichage de la carte Leaflet
+  leafletOutput("map", height = "700px")
+)
+
