9a1e06fdf668a07a484559fac2182840f041f2b0
hadrien27124 <hschmitt@etu.isara.fr>
2025-02-10 10:19:52 +0100

    Maj carte zoom paris

--- OPEN_sujet1_application/server.R
+++ OPEN_sujet1_application/server.R
@@ -4,7 +4,9 @@ library(leaflet)
 server <- function(input, output, session) {
   output$map <- renderLeaflet({
     leaflet() %>%
-      addTiles()
+      addTiles() %>%
+    setView(lng = 2.3522, lat = 48.8566, zoom = 6) %>%  
+      addMarkers(lng = 2.3522, lat = 48.8566, popup = "Paris")
         
   })
 }
