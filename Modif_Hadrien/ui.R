f35f328e5ea9490dc5a0687c0cf1ecfe049c4657
Marie <msanchez@etu.isara.fr>
2025-02-10 15:23:55 +0100

    rajout de la 3eme fenetre

--- Modif_Hadrien/ui.R
+++ Modif_Hadrien/ui.R
@@ -7,19 +7,29 @@ library(dplyr)
 
 ui <- fluidPage(
   
-  
   tabsetPanel(
     
-    tabPanel(
-      tags$div("Présentation", id = "presentation"), 
+    tabPanel("Présentation", 
+             tags$div("Bienvenue dans l'application", id = "presentation")
     ),
     
+    tabPanel("Carte", 
+             titlePanel("Carte"),
+             leafletOutput("map")
+    ),
     
-    tabPanel(
-      tags$div("Carte", id = "map"),
-      titlePanel("Carte"),
-      leafletOutput("map"),
-      
-    tabPanel(
-      tags$div("Contact", id = "contact"),
-))))
+    tabPanel("Contact", 
+             tags$div("Informations de contact", id = "contact")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      setView(lng = 2.3522, lat = 48.8566, zoom = 12) # Paris par défaut
+  })
+}
+
+shinyApp(ui, server)
