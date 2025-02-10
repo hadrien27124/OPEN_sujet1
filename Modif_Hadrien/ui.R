64345985c9c79b19f80a8d3931b6cd664bb1cbf0
hadrien27124 <hschmitt@etu.isara.fr>
2025-02-10 15:16:12 +0100

    Ajout dépot Hadrien

---
+++ Modif_Hadrien/ui.R
@@ -0,0 +1,25 @@
+library(shiny)
+library(readxl)
+library(leaflet)
+library(tidygeocoder)
+library(dplyr)
+
+
+ui <- fluidPage(
+  
+  
+  tabsetPanel(
+    
+    tabPanel(
+      tags$div("Présentation", id = "presentation"), 
+    ),
+    
+    
+    tabPanel(
+      tags$div("Carte", id = "map"),
+      titlePanel("Carte"),
+      leafletOutput("map"),
+      
+    tabPanel(
+      tags$div("Contact", id = "contact"),
+))))
