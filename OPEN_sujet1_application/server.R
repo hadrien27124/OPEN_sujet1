0e46f76b2c354833d2ce62116baef0beaae56c6d
Marie <msanchez@etu.isara.fr>
2025-02-10 10:25:37 +0100

    Ajout de Lyon

--- OPEN_sujet1_application/server.R
+++ OPEN_sujet1_application/server.R
@@ -5,8 +5,8 @@ server <- function(input, output, session) {
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
-    setView(lng = 2.3522, lat = 48.8566, zoom = 6) %>%  
-      addMarkers(lng = 2.3522, lat = 48.8566, popup = "Paris")
-        
+      setView(lng = 3.59395, lat = 47.3103, zoom = 6) %>%  # Centrage entre Paris et Lyon
+      addMarkers(lng = 2.3522, lat = 48.8566, popup = "Paris") %>%
+      addMarkers(lng = 4.8357, lat = 45.7640, popup = "Lyon")
   })
 }
