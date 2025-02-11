cd1c153f98b0cee6e562a09d3fb1f10b71f2790d
hadrien27124 <hschmitt@etu.isara.fr>
2025-02-11 09:10:40 +0100

    Fusion modif clara / Hadrien

--- Modif_Hadrien/server.R
+++ Modif_Hadrien/server.R
@@ -5,6 +5,24 @@ library(tidygeocoder)
 library(dplyr)
 library(writexl)
 
+
+# Charger le fichier Excel
+df <- read_excel("Base_de_données.xlsx")
+
+# Vérifier si les colonnes lat et long existent déjà
+if (!("lat" %in% colnames(df) && "long" %in% colnames(df))) {
+  # Géocodage uniquement si les colonnes n'existent pas
+  df <- df %>%
+    geocode(address = Adresse, method = "osm")
+  
+  # Sauvegarder le dataframe mis à jour avec lat et long dans le fichier Excel
+  write_xlsx(df, "Base_de_données.xlsx")
+}
+
+# Vérifier les noms des colonnes pour s'assurer que lat et long ont été ajoutées
+# print(colnames(df))
+
+
 server <- function(input, output, session) {
   # Création d'un objet réactif pour stocker les marqueurs
   markers <- reactiveVal(data.frame(lng = numeric(), lat = numeric()))
@@ -22,9 +40,12 @@ server <- function(input, output, session) {
   
   #Affichage de la carte
   output$map <- renderLeaflet({
-    leaflet() %>%
-    addTiles() %>%
-      setView(lng = 2.3522, lat = 48.8566, zoom = 6)
+    leaflet(df) %>%
+      addTiles() %>%  # Fond de carte
+      addMarkers(
+        lng = ~long,  # Coordonnée longitude
+        lat = ~lat,   # Coordonnée latitude
+        popup = ~paste("<b>", Nom, "</b><br/>", Adresse))  # Pop-up avec Nom + Adresse
   })
   
   # Mise à jour des marqueurs
