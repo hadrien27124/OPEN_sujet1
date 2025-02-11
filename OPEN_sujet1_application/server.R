a6b2a97ec851207fbb0acc415d990cc7c18685cd
hadrien27124 <hschmitt@etu.isara.fr>
2025-02-11 09:45:09 +0100

    MAJ OPEN PROJET1

--- OPEN_sujet1_application/server.R
+++ OPEN_sujet1_application/server.R
@@ -5,6 +5,7 @@ library(tidygeocoder)
 library(dplyr)
 library(writexl)
 
+
 # Charger le fichier Excel
 df <- read_excel("Base_de_données.xlsx")
 
@@ -21,15 +22,37 @@ if (!("lat" %in% colnames(df) && "long" %in% colnames(df))) {
 # Vérifier les noms des colonnes pour s'assurer que lat et long ont été ajoutées
 # print(colnames(df))
 
-# Création du serveur pour la carte Leaflet
+
 server <- function(input, output, session) {
+  # Création d'un objet réactif pour stocker les marqueurs
+  markers <- reactiveVal(data.frame(lng = numeric(), lat = numeric()))
+  
+  # Ajouter un marqueur
+  observeEvent(input$add_marker, {
+    new_marker <- data.frame(lng = input$longitude, lat = input$latitude)
+    markers(rbind(markers(), new_marker))  # Ajout du nouveau marqueur
+  })
+  
+  # Réinitialiser la carte (supprimer tous les marqueurs)
+  observeEvent(input$reset_map, {
+    markers(data.frame(lng = numeric(), lat = numeric()))  # Réinitialisation des marqueurs
+  })
+  
+  #Affichage de la carte
   output$map <- renderLeaflet({
     leaflet(df) %>%
       addTiles() %>%  # Fond de carte
       addMarkers(
         lng = ~long,  # Coordonnée longitude
         lat = ~lat,   # Coordonnée latitude
-        popup = ~paste("<b>", Nom, "</b><br/>", Adresse)  # Pop-up avec Nom + Adresse
-      )
+        popup = ~paste("<b>", Nom, "</b><br/>", Adresse))  # Pop-up avec Nom + Adresse
+  })
+  
+  # Mise à jour des marqueurs
+  observe({
+    leafletProxy("map") %>%
+      clearMarkers() %>%
+      addMarkers(data = markers(), ~lng, ~lat, popup = "Nouveau point")
   })
 }
+
